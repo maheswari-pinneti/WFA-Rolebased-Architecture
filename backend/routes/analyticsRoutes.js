dc072b6d55f6ab3387b1284e14bb071473bf9ca1
PinnetiMaheswari97 <pinnetimaheswari@gmail.com>
2026-08-08 23:10:23 +0530

    updated files

--- backend/routes/analyticsRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { getAnalytics } from '../controllers/analyticsController.js';
-import { authenticateToken } from '../middleware/auth.js';
-
-const router = express.Router();
-
-router.get('/', authenticateToken, getAnalytics);
-
-export default router;
